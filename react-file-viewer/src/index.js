b05e402fc8c27e325f464c86f883f18cd052e741
sebxp <sebxxxp@gmail.com>
2025-01-15 16:28:53 -0500

    solve missing dependencies

--- react-file-viewer/src/index.js
+++ react-file-viewer/src/index.js
@@ -3,8 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
+import "./index.css";
 import { store } from "./redux/store";
-import "./styles.css";
 
 const queryClient = new QueryClient();
 
