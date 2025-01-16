4d4539e8a3690564130e749eac562cec9bb7ecd1
sebxp <sebxxxp@gmail.com>
2025-01-16 09:23:49 -0500

    remove unused files - run standard in server files

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,22 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-import fileRoutes from "./routes/file.routes.js";
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import fileRoutes from './routes/file.routes.js'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const app = express();
+const app = express()
 
 // Middlewares
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '../public')))
 
 // Routes
-app.use("/files", fileRoutes);
+app.use('/files', fileRoutes)
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public", "index.html"));
-});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'))
+})
 
-export default app;
+export default app
